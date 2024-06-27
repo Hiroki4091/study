2b19fca5dec34bff90eef28fb38c0c23bd34b80b
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-06-28 08:29:46 +0900

    型：number, string, booleanを学習

--- programing/TypeScript/app.ts
+++ programing/TypeScript/app.ts
@@ -1,1 +1,16 @@
-console.log("ここに追加");
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
+    
+}
+
+let number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const ResultPhrase = 'Result: ';
+
+add(number1, number2, printResult, ResultPhrase);
