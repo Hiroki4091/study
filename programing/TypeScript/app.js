8adb272df33732420a7108ba2150e47f02bae0a1
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-06-28 09:08:07 +0900

    Array型を学習

--- programing/TypeScript/app.js
+++ programing/TypeScript/app.js
@@ -1,5 +1,13 @@
 var person = {
     name: "hiroki",
     age: 25,
+    hobbies: ['Sports', 'Cooking']
 };
+// stringの配列
+var favoriteActivities;
+favoriteActivities = ['Sports'];
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase);
+}
--- programing/TypeScript/app.ts
+++ programing/TypeScript/app.ts
@@ -1,6 +1,15 @@
 const person = {
     name : "hiroki",
     age : 25,
-}
+    hobbies : ['Sports', 'Cooking']
+};
+
+// stringの配列
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
 
 console.log(person);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase);
+}
