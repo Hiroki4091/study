10f8224091f1ed56b2f7952f791abee4b5348038
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-06-28 09:17:35 +0900

    Tuple型を学習

--- programing/TypeScript/app.ts
+++ programing/TypeScript/app.ts
@@ -1,8 +1,18 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    // Tupleの書き方
+    role: [number, string];
+} = {
     name : "hiroki",
     age : 25,
-    hobbies : ['Sports', 'Cooking']
+    hobbies : ['Sports', 'Cooking'],
+    role: [2, 'author'],
 };
+
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 
 // stringの配列
 let favoriteActivities: string[];
