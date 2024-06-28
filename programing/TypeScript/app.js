6f6e2b07af89563a8cd7bd907ff04f4e2b9d527a
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-06-28 09:37:37 +0900

    Enum型を学習

--- programing/TypeScript/app.js
+++ programing/TypeScript/app.js
@@ -1,13 +1,41 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     // Tupleの書き方
+//     role: [number, string];
+// } = {
+//     name : "hiroki",
+//     age : 25,
+//     hobbies : ['Sports', 'Cooking'],
+//     role: [2, 'author'],
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHER = 2;
+// 自動的に0から数字が割り当てられる
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHER"] = 2] = "AUTHER";
+})(Role || (Role = {}));
 var person = {
     name: "hiroki",
     age: 25,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN,
 };
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 // stringの配列
 var favoriteActivities;
-favoriteActivities = ['Sports'];
+favoriteActivities = ["Sports"];
 console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase);
+}
+if (person.role == Role.ADMIN) {
+    console.log("管理者ユーザー");
 }
--- programing/TypeScript/app.ts
+++ programing/TypeScript/app.ts
@@ -1,14 +1,35 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    // Tupleの書き方
-    role: [number, string];
-} = {
-    name : "hiroki",
-    age : 25,
-    hobbies : ['Sports', 'Cooking'],
-    role: [2, 'author'],
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     // Tupleの書き方
+//     role: [number, string];
+// } = {
+//     name : "hiroki",
+//     age : 25,
+//     hobbies : ['Sports', 'Cooking'],
+//     role: [2, 'author'],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHER = 2;
+
+// 自動的に0から数字が割り当てられる
+// 数字を設定可能（その後の数字は＋1されていく）
+// 各々の数字を指定可能
+// 文字列の指定可能
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHER = 200,
+}
+
+const person = {
+  name: "hiroki",
+  age: 25,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 };
 
 // person.role[1] = 10;
@@ -16,10 +37,14 @@ const person: {
 
 // stringの配列
 let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
+favoriteActivities = ["Sports"];
 
 console.log(person);
 
 for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase);
+  console.log(hobby.toUpperCase);
+}
+
+if (person.role == Role.ADMIN) {
+  console.log("管理者ユーザー");
 }
