05e19496577f63885d9fbd6375e289774f80466b
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-06-29 23:39:24 +0900

    unknown型を学習

--- programing/TypeScript/app.ts
+++ programing/TypeScript/app.ts
@@ -1,31 +1,10 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+// 型を指定すれば代入できる
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-
-// 戻り値を指定することができる
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-// 関数であることを指定する let combineValues: Function;
-
-// どのような引数を受け取り何を返すかを指定することができる
-let combineValues: (a: number, b: number) => number;
-
-// コールバック関数
-function addAndHandle(n1: number, n2: number, cb:(num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-});
-
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-printResult(add(5, 12));
