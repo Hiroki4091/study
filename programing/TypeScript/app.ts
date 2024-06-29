737d348f543fab678d71a75fae09c53390e948a3
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-06-30 00:03:34 +0900

    never型を学習

--- programing/TypeScript/app.ts
+++ programing/TypeScript/app.ts
@@ -2,9 +2,20 @@ let userInput: unknown;
 let userName: string;
 
 userInput = 5;
-userInput = 'Max';
+userInput = "Max";
 
 // 型を指定すれば代入できる
-if (typeof userInput === 'string') {
-    userName = userInput;
+if (typeof userInput === "string") {
+  userName = userInput;
 }
+
+// never型：戻り値を絶対に返さないと明示的に表すことができる
+function genetareError(message: string, code: number): never {
+  throw {
+    message: message,
+    error: code,
+  };
+}
+
+const result = genetareError('エラーが発生しました', 500);
+console.log(result);
