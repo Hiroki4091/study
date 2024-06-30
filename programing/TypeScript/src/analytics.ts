1f39e4c32ffe1ab28965173771f5a8bd458250b3
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-06-30 22:31:53 +0900

    ボタンを作成してクリックされるとメッセージが出力されるコードを作成

--- programing/TypeScript/src/analytics.ts
+++ programing/TypeScript/src/analytics.ts
@@ -1,1 +1,9 @@
-console.log('sending...');
+const button = document.querySelector('button')!;
+
+function clickHandler(message: string) {
+    console.log('Clicked', message);
+}
+
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, 'ようこそ'));
+}
