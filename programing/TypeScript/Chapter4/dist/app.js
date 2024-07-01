ca91bbf0d0676cb900a46f66d869fe87c11a06dd
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-07-01 23:24:52 +0900

    デフォルト値について学習

--- programing/TypeScript/Chapter4/dist/app.js
+++ programing/TypeScript/Chapter4/dist/app.js
@@ -1,11 +1,11 @@
 "use strict";
 const userName = "max";
 let age = 30;
-const add = (a, b) => a + b;
+const add = (a, b = 1) => a + b;
 const printOutput = (output) => {
     console.log(output);
 };
-printOutput(add(2, 5));
+printOutput(add(2));
 const button = document.querySelector("button");
 if (button) {
     button.addEventListener("click", (event) => {
--- programing/TypeScript/Chapter4/src/app.ts
+++ programing/TypeScript/Chapter4/src/app.ts
@@ -10,7 +10,8 @@ let age = 30;
 //     return a + b;
 // }
 
-const add = (a: number, b: number) => a + b;
+// デフォルト値：引数の後ろにイコールで値を設定（右の引数しか設定できない or 全てに設定）
+const add = (a: number, b: number = 1) => a + b;
 
 // 引数が1つの場合は引数の括弧は省略できる
 // const printOutput = (output: string | number) => console.log(output);
@@ -18,7 +19,7 @@ const printOutput: (output: string | number) => void = (output) => {
   console.log(output);
 };
 
-printOutput(add(2, 5));
+printOutput(add(2));
 
 const button = document.querySelector("button");
 
