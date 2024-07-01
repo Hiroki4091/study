b0cdcf80d7e996f78b5c02172b62f79a0d4e9331
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-07-01 23:09:29 +0900

    const, let, アロー関数について学習

--- programing/TypeScript/Chapter4/dist/app.js
+++ programing/TypeScript/Chapter4/dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("hello");
+const userName = "max";
+let age = 30;
+const add = (a, b) => a + b;
+const printOutput = (output) => {
+    console.log(output);
+};
+printOutput(add(2, 5));
+const button = document.querySelector("button");
+if (button) {
+    button.addEventListener("click", (event) => {
+        console.log(event);
+    });
+}
--- programing/TypeScript/Chapter4/src/app.ts
+++ programing/TypeScript/Chapter4/src/app.ts
@@ -1,1 +1,29 @@
-console.log("hello");
+// const 値変更不可
+const userName = "max";
+
+// 値変更可能
+let age = 30;
+
+// アロー関数(関数を短く書くことができる)
+// 処理するコードが1つの場合中括弧は省略できる,returnも省略できる
+// const add = function(a: number, b: number) {
+//     return a + b;
+// }
+
+const add = (a: number, b: number) => a + b;
+
+// 引数が1つの場合は引数の括弧は省略できる
+// const printOutput = (output: string | number) => console.log(output);
+const printOutput: (output: string | number) => void = (output) => {
+  console.log(output);
+};
+
+printOutput(add(2, 5));
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => {
+    console.log(event);
+  });
+}
