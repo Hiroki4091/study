a80cfc5ef53a03546bd2d56754efc34ea88e2b0b
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-07-03 00:04:56 +0900

    private, publicを学習

--- programing/TypeScript/class&instance/dist/app.js
+++ programing/TypeScript/class&instance/dist/app.js
@@ -1,17 +1,23 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     discribe() {
         console.log("department" + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department("Accounting");
+accounting.addEmployee(("max"));
+accounting.addEmployee(("manu"));
 accounting.discribe();
-const accountingCopy = {
-    name: "Dummy",
-    discribe: accounting.discribe
-};
-accountingCopy.discribe();
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- programing/TypeScript/class&instance/src/app.ts
+++ programing/TypeScript/class&instance/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = []; 
 
   constructor(n: string) {
     this.name = n;
@@ -8,15 +9,26 @@ class Department {
   discribe(this: Department) {
     console.log("department" + this.name);
   }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
+// Departmentクラスのインスタンス
 const accounting = new Department("Accounting");
 
+accounting.addEmployee(("max"));
+accounting.addEmployee(("manu"));
 accounting.discribe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = {
-  name: "Dummy",
-  discribe: accounting.discribe
-};
+// const accountingCopy = {
+//   name: "Dummy",
+//   discribe: accounting.discribe
+// };
 
-accountingCopy.discribe();
+// accountingCopy.discribe();
