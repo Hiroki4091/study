e16b4fb9622cc28b4e74fafd3bd927290573b3f4
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-07-05 08:52:18 +0900

    ジェネリック型を学習

---
+++ programing/TypeScript/Generics/src/app.ts
@@ -0,0 +1,15 @@
+// ジェネリック型：他の特定の型と結合された型
+// const names: Array<string> = []; // string[]と書くのと変わらない
+
+// // 型の返す値を指定できる<string> typescriptのサポートを得ることができる
+// const promise = new Promise<string>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("終わりました!");
+//   }, 2000);
+// });
+
+// // promiseがstringを返すことを指定しているのでsplitメソッドが使用できる
+// promise.then(data => {
+//     data.split(' ');
+// })
+
