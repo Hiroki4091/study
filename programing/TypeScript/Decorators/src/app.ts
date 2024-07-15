6860259c223f5e1e03586e77190ecfd8e5525089
DEGUCHIHIROKI <deguhiro4062@gmail.com>
2024-07-15 16:52:50 +0900

    便利なデコレータを学習

--- programing/TypeScript/Decorators/src/app.ts
+++ programing/TypeScript/Decorators/src/app.ts
@@ -5,9 +5,22 @@ function Logger(logString: string) {
   };
 }
 
+function WithTemplete(template: string, hookId: string) {
+  // _ : 引数は受け取るが、必要がないということを伝える
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
 // デコレータはクラスが定義されたときに実行される
 // デコレータファクトリを使用することで、デコレータの内部で行うことをカスタマイズすることができる
-@Logger("ログ出力中 - Person")
+// @Logger("ログ出力中 - Person")
+@WithTemplete("<h1>Personオブジェクト</h1>", "app")
 class Person {
   name = "John";
 
